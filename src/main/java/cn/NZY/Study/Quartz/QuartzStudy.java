82bd123b5b5cba766aa490ba4c67e486af8ed2ea
倪智勇 <nizy@getui.com>
2018-12-17 11:28:09 +0800

    init MavenProject

---
+++ src/main/java/cn/NZY/Study/Quartz/QuartzStudy.java
@@ -0,0 +1,10 @@
+package cn.NZY.Study.Quartz;
+
+/**
+ * Created by nizy on 2018/8/14.
+ */
+public class QuartzStudy {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/cn/hdu/edu/Guava/BloomFilterTest.java
@@ -0,0 +1,24 @@
+package cn.hdu.edu.Guava;
+
+import com.google.common.hash.BloomFilter;
+import com.google.common.hash.Funnels;
+
+/**
+ * Created by nizy on 2018/12/13.
+ */
+public class BloomFilterTest {
+    public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+        BloomFilter<Integer> filter = BloomFilter.create(Funnels.integerFunnel(), 10000000, 0.01);
+        for (int i = 0; i < 10000000; i++) {
+            filter.put(i);
+        }
+        System.out.println(filter.mightContain(1));
+        System.out.println(filter.mightContain(2));
+        System.out.println(filter.mightContain(3));
+        System.out.println(filter.mightContain(1000000));
+        long end = System.currentTimeMillis();
+        System.out.println("执行时间: " + (end - start));
+    }
+
+}
